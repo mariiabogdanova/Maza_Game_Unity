e251526ec72189c4a13a88348e772a4094265517
Unknown <februarymary@gmail.com>
2017-06-06 12:17:50 +0300

    Pick-ups and timer

---
+++ UnicornMaze/Assets/Scripts/Controller.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+   // public Text timeisoutText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+       // timeisoutText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Your score: " + count.ToString();
+       // if (count >= 12)
+       // {
+       //     timeisoutText.text = "You Win!";
+       // }
+    }
+}
