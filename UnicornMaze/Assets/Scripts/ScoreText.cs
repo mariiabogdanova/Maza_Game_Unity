9140ec01d4bbddb86dc9c67b87758950661af08f
Unknown <februarymary@gmail.com>
2017-06-13 14:48:32 +0300

    settings changes for all the levels, pick-ups for the first one fixed

--- UnicornMaze/Assets/Scripts/ScoreText.cs
+++ UnicornMaze/Assets/Scripts/ScoreText.cs
@@ -1,16 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
-public class ScoreText : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed;
+    public Text countText;
+   // public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+       // winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+       // if (count >= 12)
+       // {
+       //     winText.text = "You Win!";
+       // }
+    }
 }
