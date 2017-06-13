f5f8b21a3e040c9c33a8b21fda6a99a72f90a28b
Unknown <februarymary@gmail.com>
2017-06-13 14:31:09 +0300

    Auto stash before merge of "master" and "origin/master"
    level 1 nice

--- UnicornMaze/Assets/ProceduralNumberGenerator.cs
+++ UnicornMaze/Assets/ProceduralNumberGenerator.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ProceduralNumberGenerator
 {
     public static int currentPosition = 0;
-    public const string key = "123424123342421432233144441212334432121223344";
+    public const string key = "124423213412341234444431243243122142341321344221";
 
     public static int GetNextNumber()
     {
@@ -12,3 +12,4 @@ public class ProceduralNumberGenerator
         return int.Parse(currentNum);
     }
 }
+//123424123342421432233144441212334432121223344
